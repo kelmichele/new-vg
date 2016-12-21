e05a61d9cc8c9238abe66e9a0c21a299f7095fac
Kelli Parris <kelli@hsdesignhouse.com>
2016-12-21 16:56:21 -0600

    few basics, just testing the push

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+
+  	def home
+  	end
+
+	def about
+	end
+
+	def brunch_menu
+	end
+
+	def contact
+	end
+
+	def dinner_menu
+	end
+
+	def drink_menu
+	end
+
+	def happy_hour
+	end
+
+	def daily_menu
+	end
+
+	def wine_list
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  	root 'static_pages#home'
+
+  	get '/daily-menu', to: 'static_pages#daily-menu'
+  	get '/dinner-menu', to: 'static_pages#dinner-menu'
+  	get '/brunch-menu', to: 'static_pages#brunch-menu'
+  	get '/drink-menu', to: 'static_pages#drink-menu'
+  	get '/wine-list', to: 'static_pages#wine-list'
+  	get '/about', to: 'static_pages#about'
+  	get '/menus', to: 'static_pages#menus'
+  	get '/happy-hour', to: 'static_pages#happy-hour'
+  	get '/contact', to: 'static_pages#contact'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
